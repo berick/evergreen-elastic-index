2da487e97ef0eaeb6a8dca0b4668e9950bc316c3
Bill Erickson <berickxx@gmail.com>
2018-10-17 17:57:00 -0400

    Expandin availability filter; remove extra nested example
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- examples/available.sh
+++ examples/available.sh
@@ -2,6 +2,9 @@
 
 . load_config.sh
 
+# Records whose title matches 'piano' and which contain at least
+# one copy with (status 0 OR status 7) AND (cic_lib 4 OR circ_lib 5)
+
 curl -XPOST "$ES_URL/$ES_INDEX/_search?pretty=true" -d '
 {
   "query": {
@@ -15,7 +18,26 @@ curl -XPOST "$ES_URL/$ES_INDEX/_search?pretty=true" -d '
         "nested": {
           "path": "holdings",
           "query": {
-            "terms": {"holdings.status": ["0", "7"]}
+            "bool": {
+              "must": [
+                {
+                  "bool": {
+                    "should": [
+                      {"term": {"holdings.status": "0"}},
+                      {"term": {"holdings.status": "7"}}
+                    ]
+                  }
+                },
+                {
+                  "bool": {
+                    "should": [
+                      {"term": {"holdings.circ_lib": "4"}},
+                      {"term": {"holdings.circ_lib": "5"}}
+                    ]
+                  }
+                }
+              ]
+            }
           }
         }
       }
@@ -42,13 +64,12 @@ curl -XPOST "$ES_URL/$ES_INDEX/_search?pretty=true" -d '
         "path": "holdings"
       },
       "aggs": {
-        "availability": {
+        "copy_status": {
           "terms": {"field": "holdings.status"}
         },
-        "location": {
+        "copy_location": {
           "terms": {"field": "holdings.location"}
         }
-
       }
     },
     "type_of_resource": {
